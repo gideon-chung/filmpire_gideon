fb597b5e2b7e911bdb48f505fc5cbedba00667b7
Gideon Chung <gideon.j.chung@gmail.com>
2022-05-31 20:26:22 -0400

    more components added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import { CssBaseline } from '@mui/material'
+import { Route, Switch } from 'react-router-dom'
 
-const App = () => {
+function App() {
     return (
-        <div>App</div>
-    )
+        <div>
+            <CssBaseline />
+            <main>
+                <Switch>
+                    <Route exact path="/movie/:id">
+                        <h1>Movie Informations</h1>
+                    </Route>
+                    <Route exact path="/actors/:id">
+                        <h1>Actors</h1>
+                    </Route>
+                    <Route exact path="/">
+                        <h1>Movies</h1>
+                    </Route>
+                    <Route exact path="/profile/:id">
+                        <h1>Profile</h1>
+                    </Route>
+                </Switch>
+            </main>
+        </div>
+    );
 }
 
-export default App
+export default App;
